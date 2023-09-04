f51f51d47120b6f9f5207777b522f3e9d008cbf4
Anas Ahmed <123072865+anasahmed2@users.noreply.github.com>
2023-09-04 13:43:00 -0700

    Add files via upload

--- component/Cards/index.js
+++ component/Cards/index.js
@@ -1,10 +1,15 @@
-const Card = ({carContainer,layoutCard,cardImageContainer ,cardDataContainer}) => {
+const Card = ({ cardContainer, layoutCard, cardImageContainer, cardDataContainer, heading, textgreadent1,headingvalue ,headingvalueG,paragvalue}) => {
     return (
         <>
-            <div className={carContainer}>
-                <div className={layoutCard}>
-                    <div className={cardImageContainer}></div>
-                    <div className={cardDataContainer}></div>
+            <div className="container">
+                <div className={cardContainer}>
+                    <div className={layoutCard}>
+                        <div className={cardImageContainer}></div>
+                        <div className={cardDataContainer}>
+                            <div className={heading}><h1>{headingvalue} <span className={textgreadent1}>{headingvalueG}</span>.</h1></div>
+                            <div className={heading}><p>{paragvalue} </p></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
